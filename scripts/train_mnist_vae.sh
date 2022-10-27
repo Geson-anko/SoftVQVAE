58a11e4415726b1ce0fcf2a756289d832a722c1d
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-10-27 13:09:39 +0900

    ADD training script of mnist_vae

---
+++ scripts/train_mnist_vae.sh
@@ -0,0 +1,11 @@
+python src/train.py \
+    datamodule=mnist_all_to_training \
+    model=mnist_vae \
+    callbacks=for_mnist_vae \
+    logger=tensorboard \
+    tags="[SoftVQ, mnist_vae]" \
+    test=False \
+    task_name="mnist_vae" \
+    trainer.max_epochs=30 \
+    model.r_loss_scaler=10000
+

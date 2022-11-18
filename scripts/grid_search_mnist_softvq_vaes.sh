7f9c67074a88193d08b94f3cdaea42e85ae073fd
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-11-18 20:36:51 +0900

    ADD shell script for grid search

---
+++ scripts/grid_search_mnist_softvq_vaes.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+python src/train.py -m \
+    hparams_search=mnist_gird_search_softvq_vae \
+    experiment=mnist_softvq_vae \
+    trainer.accelerator=gpu \
+    task_name=mnist-softvq-vae-grid-search 
+
+python src/train.py -m \
+    hparams_search=mnist_gird_search_softvq_vae \
+    experiment=mnist_softvq_vae \
+    model=image_dot_product_softvq_vae \
+    tags="[SoftVQ VAE, image_simple_softvq_vae, mnist, dot product softvq]" \
+    trainer.accelerator=gpu \
+    task_name=mnist-softvq-vae-grid-search 
+
+
+python src/train.py -m \
+    hparams_search=mnist_gird_search_straight_softvq_vae \
+    experiment=mnist_straight_dot_product_softvq_vae_with_bn \
+    trainer.accelerator=gpu \
+    task_name=mnist-straight-softvq-vae-grid-search 
+
+python src/train.py -m \
+    hparams_search=mnist_gird_search_straight_softvq_vae \
+    experiment=mnist_straight_softvq_vae \
+    trainer.accelerator=gpu \
+    task_name=mnist-straight-softvq-vae-grid-search 
+
+
+python src/train.py -m \
+    hparams_search=mnist_gird_search_straight_softvq_vae \
+    experiment=mnist_straight_dot_product_softvq_vae_with_bn \
+    trainer.accelerator=gpu \
+    task_name=mnist-straight-softvq-vae-grid-search \
+    model.kl_div_loss_scaler=0.01
+
+python src/train.py -m \
+    hparams_search=mnist_gird_search_straight_softvq_vae \
+    experiment=mnist_straight_softvq_vae \
+    trainer.accelerator=gpu \
+    task_name=mnist-straight-softvq-vae-grid-search \
+    model.kl_div_loss_scaler=0.01

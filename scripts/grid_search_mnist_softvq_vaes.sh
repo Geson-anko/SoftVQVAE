8473e129ca556871d74ac14427efc81f93c23817
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2023-02-11 13:32:37 +0900

    Format by pre-commit

--- scripts/grid_search_mnist_softvq_vaes.sh
+++ scripts/grid_search_mnist_softvq_vaes.sh
@@ -4,7 +4,7 @@ python src/train.py -m \
     hparams_search=mnist_gird_search_softvq_vae \
     experiment=mnist_softvq_vae \
     trainer.accelerator=gpu \
-    task_name=mnist-softvq-vae-grid-search 
+    task_name=mnist-softvq-vae-grid-search
 
 python src/train.py -m \
     hparams_search=mnist_gird_search_softvq_vae \
@@ -12,20 +12,20 @@ python src/train.py -m \
     model=image_dot_product_softvq_vae \
     tags="[SoftVQ VAE, image_simple_softvq_vae, mnist, dot product softvq]" \
     trainer.accelerator=gpu \
-    task_name=mnist-softvq-vae-grid-search 
+    task_name=mnist-softvq-vae-grid-search
 
 
 python src/train.py -m \
     hparams_search=mnist_gird_search_straight_softvq_vae \
     experiment=mnist_straight_dot_product_softvq_vae_with_bn \
     trainer.accelerator=gpu \
-    task_name=mnist-straight-softvq-vae-grid-search 
+    task_name=mnist-straight-softvq-vae-grid-search
 
 python src/train.py -m \
     hparams_search=mnist_gird_search_straight_softvq_vae \
     experiment=mnist_straight_softvq_vae \
     trainer.accelerator=gpu \
-    task_name=mnist-straight-softvq-vae-grid-search 
+    task_name=mnist-straight-softvq-vae-grid-search
 
 
 python src/train.py -m \
